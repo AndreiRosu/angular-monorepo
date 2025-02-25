cab8150e9796894d4ac0f76bc23b06487afc1a7f
Andrei Rosu <8149965+AndreiRosu@users.noreply.github.com>
2025-02-25 13:29:39 +0200

    base monorepo

--- apps/inventory/src/app/app.component.ts
+++ apps/inventory/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ProductsComponent } from '@angular-monorepo/products';
 
 @Component({
-  imports: [RouterModule],
+  imports: [RouterModule, ProductsComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'angular-monorepo';
+  title = 'inventory';
 }
